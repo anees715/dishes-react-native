dcb1874899bd49c979abbef6a7ccf2f20192a9d5
anees715 <anees715@gmail.com>
2018-11-24 00:53:09 +0530

    add header styling

--- src/navigations/UserStack.js
+++ src/navigations/UserStack.js
@@ -1,9 +1,25 @@
 import { createStackNavigator, createDrawerNavigator, createBottomTabNavigator} from 'react-navigation';
 import HomeScreen from '../screens/HomeScreen';
 
-const mainStack = createStackNavigator({ 
-  HomeScreen: HomeScreen 
-});
+const mainStack = createStackNavigator(
+  { 
+  HomeScreen: HomeScreen
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#f39c12',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        alignSelf: 'center',
+        color: '#ffffff'
+      },
+    },
+  }
+);
 
 const tabStack = createBottomTabNavigator({
   Home: {screen: mainStack}
